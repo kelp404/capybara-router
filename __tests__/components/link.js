26c4ebdf7cc654d7ad572c4451e36b4279ebfde6
Kelp <kelp@phate.org>
2020-04-05 14:38:12 +0800

    test: replace coffeescript with javascript at link.js

---
+++ __tests__/components/link.js
@@ -0,0 +1,62 @@
+const history = require('history');
+const React = require('react');
+const renderer = require('react-test-renderer');
+const Route = require('../../lib/route');
+const utils = require('../../lib/utils');
+const {Router, Link} = require('../../');
+
+let router = null;
+beforeEach(() => {
+  router = new Router({
+    history: history.createMemoryHistory({initialEntries: ['/']}),
+    routes: [
+      {name: 'home', uri: '/'}
+    ]
+  });
+});
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+
+test('Link component render.', () => {
+  const component = renderer.create(<Link to="https://github.com">GitHub</Link>);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Link component render with object props.', () => {
+  const route = new Route({
+    name: 'web',
+    uri: '/web?index'
+  });
+  jest.spyOn(utils, 'findRouteByNameInRoutes').mockImplementation(name => {
+    expect(name).toBe('web');
+    return route;
+  });
+  jest.spyOn(route, 'generateUri').mockImplementation(() => '/web?index=0');
+  const component = renderer.create(
+    <Link to={{name: 'web', params: {index: 0}}}>Web</Link>
+  );
+  const tree = component.toJSON();
+  expect(utils.findRouteByNameInRoutes).toBeCalled();
+  expect(route.generateUri).toBeCalledWith({index: 0});
+  expect(tree).toMatchSnapshot();
+});
+
+test('Link component calls router.go() when it was clicked.', () => {
+  router.go = jest.fn(() => {});
+  const component = renderer.create(<Link to="https://github.com">GitHub</Link>);
+  const tree = component.toJSON();
+  tree.props.onClick(new Event('click'));
+  expect(router.go).toBeCalledWith('https://github.com');
+});
+
+test('Link component does not call router.go() when it was clicked with the meta key.', () => {
+  router.go = jest.fn(() => {});
+  const component = renderer.create(<Link to="https://github.com">GitHub</Link>);
+  const tree = component.toJSON();
+  const event = new Event('click');
+  event.metaKey = true;
+  tree.props.onClick(event);
+  expect(router.go).not.toBeCalled();
+});
